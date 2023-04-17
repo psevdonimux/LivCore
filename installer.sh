607bffd43cbd55b23c9c39d6bcf69125080f06b9
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2023-04-17 22:55:02 +0300

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+apt install tar unzip wget
+name="LivCore"
+git clone https://github.com/psevdonimux/$name.git
+wget https://github.com/pmmp/PHP-Binaries/releases/download/php-8.0-latest/PHP-Linux-x86_64-PM4.tar.gz
+mv $name/* $PWD
+unzip src.zip
+mv PHP-Linux-x86_64-PM4.tar.gz bin.gz
+tar -xvf bin.gz
+chmod 777 ./start.sh
+rm -r $name src.zip bin.gz LICENSE README.md installer.sh
