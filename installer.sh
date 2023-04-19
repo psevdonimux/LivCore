72ef97a5f5da217b5ac6513ece7e3a2bcb50ab15
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2023-04-19 11:10:41 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 apt install tar unzip wget git
 name="LivCore"
+name2="PHP-Linux-x86_64-PM4.tar.gz"
 git clone https://github.com/psevdonimux/$name.git
-wget https://github.com/pmmp/PHP-Binaries/releases/download/php-8.0-latest/PHP-Linux-x86_64-PM4.tar.gz
+wget https://github.com/pmmp/PHP-Binaries/releases/download/php-8.0-latest/$name2
 mv $name/* $PWD
 unzip src.zip
-mv PHP-Linux-x86_64-PM4.tar.gz bin.gz
-tar -xvf bin.gz
+tar -xvf $name2
 chmod 777 ./start.sh
-rm -r $name src.zip bin.gz LICENSE README.md installer.sh
+rm -r $name src.zip $name2 LICENSE README.md installer.sh
