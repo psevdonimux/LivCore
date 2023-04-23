4b1a3ea500f314b30e420240397d8d74d395173f
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2023-04-23 19:28:33 +0300

    убрал распаковку ядра в формате zip

--- installer.sh
+++ installer.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-apt install tar unzip wget git
+apt install tar wget git
 name="LivCore"
 name2="PHP-Linux-x86_64-PM4.tar.gz"
 git clone https://github.com/psevdonimux/$name.git
 wget https://github.com/pmmp/PHP-Binaries/releases/download/php-8.0-latest/$name2
 mv $name/* $PWD
-unzip src.zip
 tar -xvf $name2
 chmod 777 ./start.sh
 rm -r $name src.zip $name2 LICENSE README.md installer.sh
