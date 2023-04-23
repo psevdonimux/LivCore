6dda2301a631f5e069cab33450dae16a38737ae6
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2023-04-23 18:57:40 +0300

    Add files via upload

---
+++ src/pocketmine/level/generator/PopulationTask.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace pocketmine\level\generator;
+
+use pocketmine\level\format\Chunk;
+use pocketmine\level\{Level, SimpleChunkManager};
+use pocketmine\scheduler\AsyncTask;
+use pocketmine\Server;
+
+class PopulationTask extends AsyncTask{
+
+ public bool $state;
+ public int $levelId;
+ public string $chunk, $chunk0, $chunk1, $chunk2, $chunk3, $chunk5, $chunk6, $chunk7, $chunk8;
+
+ public function __construct(Level $level, Chunk $chunk){
+  $this->state = true;
+  $this->levelId = $level->getId();
+  $this->chunk = $chunk->fastSerialize();
+ }
+ public function onRun(){
+  $manager = $this->getFromThreadStore('generation.level'. $this->levelId. '.manager');
+  $generator = $this->getFromThreadStore('generation.level'. $this->levelId. '.generator');
+  if(!($manager instanceof SimpleChunkManager) or !($generator instanceof Generator)){
+   $this->state = false;
+   return;
+  }
+  $chunk2 = Chunk::fastDeserialize($this->chunk);
+  $x = $chunk2->getX();
+  $z = $chunk2->getZ();
+  $manager->setChunk($x, $z, $chunk2);
+  $chunk = $manager->getChunk($x, $z);
+  if(!$chunk2->isGenerated()){
+   $generator->generateChunk($x, $z);  
+   $chunk->setGenerated();
+  }
+  $generator->populateChunk($chunk->getX(), $chunk->getZ());
+  $chunk->setPopulated();
+  $chunk->recalculateHeightMap();
+  $chunk->populateSkyLight();
+  $chunk->setLightPopulated();
+  $this->chunk = $chunk->fastSerialize();
+  $manager->cleanChunks();
+ }
+ public function onCompletion(Server $server){
+  $level = $server->getLevel($this->levelId);
+   if($level !== null){
+  if(!$this->state){
+   $level->registerGenerator();
+  }
+  $chunk = Chunk::fastDeserialize($this->chunk);
+  $level->generateChunkCallback($chunk->getX(), $chunk->getZ(), $this->state ? $chunk : null);
+   }
+ }
+}
