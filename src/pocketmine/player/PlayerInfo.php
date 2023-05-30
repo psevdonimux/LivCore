c1a15f6f40e2e1bfe6ddf117231f4ec71bdb2740
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2023-05-30 12:11:51 +0300

    обновил PlayerInfo.php

--- src/pocketmine/player/PlayerInfo.php
+++ src/pocketmine/player/PlayerInfo.php
@@ -3,7 +3,7 @@
 namespace pocketmine\player;
 
 use pocketmine\network\mcpe\protocol\DataPacket;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\{TextFormat, UUID};
 
 class PlayerInfo{
 
@@ -15,7 +15,40 @@ class PlayerInfo{
  public function getPacket() : DataPacket{
   return $this->packet;
  }
- public function getName() : string{
-  return TextFormat::clean($this->packet->username);
+ public function getUsername() : string{
+  return TextFormat::clean($this->getPacket()->username);
+ }
+ public function getLowerCaseName() : string{
+  return strtolower($this->getName());
+ }
+ public function getProtocol() : int{
+  return $this->getPacket()->protocol;
+ }
+ public function getDeviceModel() : string{
+  return $this->getPacket()->deviceModel;
+ } 
+ public function getDeviceOs() : int{
+  return $this->getPacket()->deviceOS;
+ }
+ public function getClientId() : int{
+  return $this->getPacket()->clientId;
+ }
+ public function getLanguageCode() : string{
+  return $this->getPacket()->languageCode;
+ } 
+ public function getXuid() : ?string{
+  return $this->getPacket()->identityPublicKey;
+ }
+ public function getUniqueId() : string{
+  return UUID::fromString($this->getPacket()->clientUUID);
+ }
+ public function getRawUniqueId() : string{
+  return $this->getUuid()->toBinary();
+ }
+ public function getSkinData() : string{
+  return $this->getPacket()->skin;
+ }
+ public function getSkinId() : string{
+  return $this->getPacket()->skinId;
  }
 }
